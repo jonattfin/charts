cb95aee3297006ce2628e8bd3c9ed3d9faa8b4c2
jonattfin <jonattfin@gmail.com>
2019-05-15 13:26:21 +0300

    wip

--- src/api/realApi.js
+++ src/api/realApi.js
@@ -2,12 +2,6 @@ import { UradService } from './serviceFactory';
 
 export default class RealApi {
   static async fetchMeasures() {
-    const config = {
-      'X-User-id': 'www',
-      'X-User-hash': 'global',
-      'Origin': 'https://www.uradmonitor.com'
-    };
-
-    return UradService.get(`devices`, config);
+    return UradService.get(`devices`);
   }
 }
